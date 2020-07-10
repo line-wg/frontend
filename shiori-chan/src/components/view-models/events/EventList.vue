2cd01a02fd2f6d2ca74563040446284039cdcf4b
kawachi.shusaku <kawachi.shusaku@opensesame-tech.com>
2020-07-10 15:46:02 +0900

    イベント一覧画面一部作成 containerとview-modelの切り分けのみ完了

---
+++ shiori-chan/src/components/view-models/events/EventList.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+  div
+    v-tabs(
+      v-model="tab"
+      grow
+    )
+      v-tab(
+        v-for="( content , index ) in contents"
+        :key="index"
+      ) {{ content.name }}
+      v-tabs-items(
+        v-model="tab"
+      )
+</template>
+<script>
+export default {
+  data:()=>({
+    tab: null ,
+    contents: [
+      {
+        name:"参加中のイベント" ,
+      } ,
+      {
+        name:"参加できるイベント" ,
+      } ,
+      {
+        name:"あなたにおすすめのイベント" ,
+      } ,
+      {
+        name:"終了したイベント" ,
+      } ,
+    ]
+  }) ,
+}
+</script>
+<style lang="scss">
+</style>
--- shiori-chan/src/containers/events/List.vue
+++ shiori-chan/src/containers/events/List.vue
@@ -1,11 +1,31 @@
 <template lang="pug">
-  div イベント一覧画面
+  div
+    participant-event-button
+    block-button( 
+      class="mb-2"
+      text="新しくイベントを作る"
+      @click="nextTo('EventCreate')" 
+    )
+    event-list
 </template>
 <script>
+import BlockButton from '../../components/designs/buttons/BlockButton'
+import EventList from '../../components/view-models/events/EventList'
+import ParticipantEventButton from '../../components/designs/events/ParticipationEventButton'
 export default {
+  components:{
+    BlockButton ,
+    EventList ,
+    ParticipantEventButton ,
+  },
   created(){
     this.$store.dispatch( 'SideMenu/canShowing' , true )
   },
+  methods:{
+    nextTo( name ){
+      this.$router.push( { name } )
+    }
+  }
 }
 </script>
 <style lang="scss">
